9317afb92232d626289c03e13e80d00dc5a5dc36
Gavin Ong <gavinong10@gmail.com>
2017-12-23 15:44:05 +1000

    Add content form id

--- wp-content/themes/marketingresults/template-parts/content-contact.php
+++ wp-content/themes/marketingresults/template-parts/content-contact.php
@@ -166,7 +166,7 @@
 
             <div class="row">
 
-            <div class="col-md-offset-2 col-md-8">
+            <div id="content-form" class="col-md-offset-2 col-md-8">
 
             	<?php echo do_shortcode('[contact-form-7 id="187" title="Contact form"]'); ?>
 
