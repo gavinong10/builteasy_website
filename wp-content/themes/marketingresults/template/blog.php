29de43be17d89f85299bded53ac83870907e102f
Gavin Ong <gavinong10@gmail.com>
2017-12-23 16:14:43 +1000

    Add blog

---
+++ wp-content/themes/marketingresults/template/blog.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: Blog */
+
+get_header();?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main blog">
+
+			<?php
+            // while (have_posts()): the_post();
+
+            //     get_template_part('template-parts/content', 'blog');
+
+            //     // If comments are open or we have at least one comment, load up the comment template.
+            // endwhile; // End of the loop.
+            ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+    get_footer();
+?>
