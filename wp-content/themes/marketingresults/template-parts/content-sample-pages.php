44f63641b7293de70b342c808093453b6d584e60
Gavin Ong <gavinong10@gmail.com>
2017-12-29 22:13:44 +1000

    text-decoration=none;

--- wp-content/themes/marketingresults/template-parts/content-sample-pages.php
+++ wp-content/themes/marketingresults/template-parts/content-sample-pages.php
@@ -280,7 +280,7 @@
 
                 <?php the_field('five_content');?>
 				<?php $five_url=get_field('five_button_url'); ?>
-            	<a class="btn btn-sm btn-danger text-white" href="<?php echo $five_url; ?>" target="_blank"><?php the_field('five_button'); ?></a>
+            	<a style="text-decoration: none;" class="btn btn-sm btn-danger text-white" href="<?php echo $five_url; ?>" target="_blank"><?php the_field('five_button'); ?></a>
 
             </div>
 
